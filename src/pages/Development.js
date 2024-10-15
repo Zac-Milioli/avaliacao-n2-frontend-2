3b834a0e9e337f44e8ac484948b246226d7f2af0
Zac-Milioli <zacmilioli@gmail.com>
2024-10-14 21:10:37 -0300

    update: Design das páginas concluído

--- src/pages/Development.js
+++ src/pages/Development.js
@@ -5,8 +5,42 @@ import Footer from '../components/Footer';
 const Development = () => (
     <div>
         <Header />
-        <h1>Desenvolvimento</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <div className='wikiPage'>
+            <h1>Desenvolvimento</h1>
+            <div className='wikiTextCard'>
+                <p>
+                    O desenvolvimento do meu site com múltiplas páginas foi um processo meticuloso e organizado, seguindo uma sequência lógica para garantir a consistência e a funcionalidade da aplicação. Abaixo está uma descrição detalhada do processo de desenvolvimento:
+                </p>
+                <h2>Design Inicial das Páginas</h2>
+                <p>
+                    O primeiro passo foi desenvolver o design de todas as páginas. Comecei pela página de boas-vindas, que é a primeira interação do usuário com o site. Esta página foi projetada para ser simples e acolhedora, com uma animação de fade-in para o título de boas-vindas e um botão de login.
+                </p>
+                <p>
+                    Em seguida, desenvolvi a página de login, que inclui campos de entrada para o nome e a senha do usuário, além de um botão para fazer login. A lógica para redirecionar o usuário para a página "Sobre o Zac" após o login foi implementada posteriormente.
+                </p>
+                <p>
+                    Após a página de login, criei a página "Sobre o Zac", que inclui um cabeçalho e um rodapé fixos, além de links de navegação para as outras páginas. Esta página foi preenchida com a história acadêmica e profissional resumida do Zac.
+                </p>
+                <p>
+                    A próxima página desenvolvida foi a "Avaliação N2", que segue o mesmo layout da página "Sobre o Zac", com cabeçalho e rodapé fixos e como conteúdo a explicação da avaliação N2 de Frontend 2.
+                </p>
+                <p>
+                    A última página a ser projetada foi a página "Desenvolvimento", que também segue o mesmo layout das páginas anteriores e inclui uma descrição detalhada do processo de desenvolvimento do site.
+                </p>
+                <h2>Implementação da Lógica de Login</h2>
+                <p>
+                    Após o design inicial de todas as páginas, implementei a lógica para o login. Esta é apenas uma lógica simples que verifica se o usuário e senha se igualam aos registrados previamente em um JSON do sistema, permitindo que o usuário visualize as wikis caso válido.
+                </p>
+                <h2>Estrutura do Projeto</h2>
+                <p>
+                    Todo o projeto foi construído utilizando React, com as páginas separadas em diferentes arquivos para facilitar a manutenção e a escalabilidade. Além disso, os componentes reutilizáveis, como o cabeçalho (Header) e o rodapé (Footer), foram criados separadamente e importados nas páginas conforme necessário.
+                </p>
+                <h2>Design e Estilo</h2>
+                <p>
+                    O design escolhido para o site foi baseado em um tema escuro, utilizando uma paleta de cores que inclui preto, #2D0029, #cf3097 e #ecafd7. Algumas páginas apresentam um fundo com gradiente para adicionar um toque visual atraente. A consistência nas cores e no estilo foi mantida em todas as páginas para proporcionar uma experiência de usuário coesa.
+                </p>
+            </div>
+        </div>
         <Footer />
     </div>
 );
--- src/pages/Evaluation.js
+++ src/pages/Evaluation.js
@@ -5,8 +5,25 @@ import Footer from '../components/Footer';
 const Evaluation = () => (
     <div>
         <Header />
-        <h1>Avaliação N2</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <div className='wikiPage'>
+            <h1>Avaliação N2</h1>
+            <p>
+                A "Avaliação N2" da matéria de "Frontend 2" consiste em criar uma aplicação em ReactJS com base nos conteúdos vistos até o momento na disciplina. A aplicação deve atender aos seguintes requisitos:
+            </p>
+            <ul>
+                <li>A aplicação deve ter menu e páginas.</li>
+                <li>Uma das páginas deve ter um formulário.</li>
+                <li>O trabalho pode ser algo relacionado ao PMI (Projeto Multidisciplinar Integrador).</li>
+            </ul>
+            <p>
+                No entanto, as páginas desenvolvidas nesta aplicação não são referentes ao PMI. A aplicação desenvolvida é simples e contém as seguintes páginas:
+            </p>
+            <ul>
+                <li>Uma página de boas-vindas.</li>
+                <li>Uma página de login.</li>
+                <li>Três páginas de wiki, acessíveis apenas após o login.</li>
+            </ul>
+        </div>
         <Footer />
     </div>
 );
